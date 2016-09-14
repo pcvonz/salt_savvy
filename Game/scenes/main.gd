9940a5cf655be90c527b8bc948ad511cb0f2bdff
Paul Von Zimmerman <pcvonz@gmail.com>
2016-09-13 20:31:48 -0700

    First commikt

---
+++ Game/scenes/main.gd
@@ -0,0 +1,16 @@
+
+extends Spatial
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+var obs
+
+func _ready():
+	get_node("Spatial/level1/col").connect("input_event", self, "play_level_one", [get_node("Spatial/level1/col")])
+	obs = get_node("Observer")
+func play_level_one(camera, event, click_pos, click_normal, shape_idx, object):
+	var dist = obs.get_global_transform().origin.distance_to(object.get_global_transform().origin)
+	print(dist)
+	if(event.is_action_pressed("click") and dist < 4):
+		get_tree().change_scene("res://scenes/level1.tscn")
---
+++ Game/scripts/salt.gd
@@ -0,0 +1,58 @@
+
+extends Node2D
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+var delta
+export var salt_time = 5.0
+export var error_range = 2.0
+export var win_text = "Congratulations, you pro salter! Press the salt button to continue"
+var salting = false
+var time = 0
+var salted = false 
+var error = 1212
+var salt_par
+var completed = false
+var bar
+var down = false
+func _ready():
+	set_process(true)
+	set_process_input(true)
+	get_node("Label").set_text(str("Salt for ", str(salt_time), " seconds"))
+	salt_par = get_node("Particles2D")
+	salt_par.set_emitting(false)
+	bar = get_node("ProgressBar")
+
+func _process(delta):
+	if not completed:
+		if(salting == true):
+			if(time >= bar.get_max() and down == false):
+				down = true
+			elif(time <= bar.get_min() and down == true):
+				down = false
+			
+			if(down == false):
+				time = time + delta
+			else:
+				time = time - delta
+			bar.set_value(time)
+		else:
+			bar.set_value(0)
+	else:
+		if Input.is_action_pressed("salt"):
+			get_tree().change_scene("res://scenes/main.tscn")
+
+
+func _input(event):
+	if(event.is_action_pressed("salt")):
+		salting = true
+		get_node("Label").set_text("")
+		salt_par.set_emitting(false)
+	if(event.is_action_released("salt")):
+		var error = (bar.get_value() / bar.get_max())* 100
+		salting = false
+		time = 0
+		if(error >= 93):
+			salt_par.set_emitting(true)
+			completed = true
