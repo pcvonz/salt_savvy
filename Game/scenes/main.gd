1783c710cf8ccde76516f4b965aed654197867a8
Paul Von Zimmerman <pcvonz@gmail.com>
2016-09-15 16:51:38 -0700

    Version .1 what up

--- Game/scenes/main.gd
+++ Game/scenes/main.gd
@@ -7,10 +7,9 @@ extends Spatial
 var obs
 
 func _ready():
-	get_node("Spatial/level1/col").connect("input_event", self, "play_level_one", [get_node("Spatial/level1/col")])
+	get_node("Spatial/level/col").connect("input_event", self, "play_level_one", [get_node("Spatial/level/col")])
 	obs = get_node("Observer")
 func play_level_one(camera, event, click_pos, click_normal, shape_idx, object):
 	var dist = obs.get_global_transform().origin.distance_to(object.get_global_transform().origin)
-	print(dist)
 	if(event.is_action_pressed("click") and dist < 4):
 		get_tree().change_scene("res://scenes/level1.tscn")
--- Game/scripts/salt.gd
+++ Game/scripts/salt.gd
@@ -8,6 +8,7 @@ var delta
 export var salt_time = 5.0
 export var error_range = 2.0
 export var win_text = "Congratulations, you pro salter! Press the salt button to continue"
+export var amount_needed = 5
 var salting = false
 var time = 0
 var salted = false 
@@ -18,16 +19,25 @@ var bar
 var down = false
 var anim_time = 0
 var anim_played = false
-
+var food
+var curr_food
+var count = 0
+var label
 func _ready():
 	set_process(true)
 	set_process_input(true)
 	#get_node("Label").set_text(str("Salt for ", str(salt_time), " seconds"))
-	salt_par = get_node("Particles2D")
-	salt_par.set_emitting(false)
+	salt_par = preload("res://scenes/salt_particle.tscn").instance()
+	food = preload("res://scenes/chowder.tscn").instance()
 	bar = get_node("ProgressBar")
+	bar.set_max(rand_range(.2, 1))
+	add_food()
+	label = get_node("Label")
 
 func _process(delta):
+	label.set_text(str("Salted: ", count))
+	if amount_needed <= count:
+		get_tree().change_scene("res://scenes/main.tscn")
 	if not completed:
 		if(salting == true):
 			if(time >= bar.get_max() and down == false):
@@ -44,23 +54,47 @@ func _process(delta):
 			bar.set_value(0)
 	else:
 		anim_time += delta
-		if(anim_time > 4):
-			if not get_node("../food/AnimationPlayer").is_playing() and not anim_played:
-				get_node("../food/AnimationPlayer").play("food exit")
+		if(anim_time > .75):
+			if not curr_food.get_node("AnimationPlayer").is_playing() and not anim_played:
+				curr_food.get_node("AnimationPlayer").play("food exit")
 				anim_played = true
-		if Input.is_action_pressed("salt"):
-			get_node("../food/AnimationPlayer").play("food exit")
 
+		if(anim_time > 1.25):
+			count += 1
+			add_food()
+			randomize()
+			bar.set_max(rand_range(.2, 1))
+			anim_played = false
+			anim_time = 0
+			completed = false
+			error = 100
+				
+
+func shoot_salt(speed):
+	var salt_amount = 20
+	var offset = salt_amount / 2
+	for i in range(0, salt_amount):
+		var temp_salt = salt_par.duplicate()
+		curr_food.add_child(temp_salt)
+		temp_salt.set_pos(get_node("Position2D").get_pos()+ Vector2(offset, sin(offset)* 50))
+		temp_salt.apply_impulse(temp_salt.get_pos(), (Vector2(0, 1000/speed)))
+		offset += 1
+
+func add_food():
+	if(curr_food):
+		curr_food.queue_free()
+	curr_food = food.duplicate()
+	print(curr_food.get_pos())
+	add_child(curr_food)
 
 func _input(event):
 	if(event.is_action_pressed("salt")):
 		salting = true
 		get_node("Label").set_text("")
-		salt_par.set_emitting(false)
 	if(event.is_action_released("salt")):
 		var error = (bar.get_value() / bar.get_max())* 100
 		salting = false
 		time = 0
-		if(error >= 93):
-			salt_par.set_emitting(true)
+		if(error >= 93 and not completed):
+			shoot_salt(bar.get_value())
 			completed = true
